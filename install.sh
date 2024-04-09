06b582cc61f2cb56d3d3f73b208cc5b7455e4e3a
Matte <51192395+MatteZ02@users.noreply.github.com>
2024-04-09 14:42:03 +0300

    First setup

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ ! "$BASH_VERSION" ] ; then
+    exit 1
+fi
+
+echo "
+==============================
+
+Multi Platform Project - Ansible
+Install Script
+
+------------------------------
+"
+
+stop () {
+
+echo "
+==============================
+"
+
+exit 1
+
+}
+
+mkdir -p ~/.ssh &> /dev/null
+
+apt-get install -y python3-pip python3-venv jq git curl &> /dev/null
+python3 -m venv /opt/ansible &> /dev/null
+/opt/ansible/bin/pip3 install ansible hvac netaddr jmespath pexpect &> /dev/null
+
+/opt/ansible/bin/ansible-galaxy collection install -r requirements.yml --upgrade &> /dev/null
+
+/opt/ansible/bin/ansible-pull -U ssh://git@github.com/MatteZ02/mpp-ansible --private-key ~/.ssh/id_rsa tasks.yml -t install
+
+
+echo "
+==============================
+"
