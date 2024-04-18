a232ca888ff191a5542291353af749641fb3da9a
Matte <51192395+MatteZ02@users.noreply.github.com>
2024-04-18 17:50:57 +0300

    update

---
+++ files/certbot/nginx.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo -n "$CERTBOT_VALIDATION" > /root/nginx/html/.well-known/acme-challenge/$CERTBOT_TOKEN
+/opt/ansible/bin/ansible-pull -U ssh://git@github.com/MatteZ02/mpp-ansible --private-key ~/.ssh/id_rsa tasks.yml -t nginx
